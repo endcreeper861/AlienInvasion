3a01ae11756a396f3dd34165eaa1970a021d409b
endcreeper861 <3130450154@qq.com>
2022-06-05 21:45:23 +0800

    添加飞船左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         """开始游戏的主循环。"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
             # 让最近绘制的屏幕可见
@@ -39,7 +40,17 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False    
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False        
 
 
 if __name__ == "__main__":
