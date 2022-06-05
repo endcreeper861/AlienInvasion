4be96f22f33d1f9f8a9ed90f2a299be8fcc24b41
endcreeper861 <3130450154@qq.com>
2022-06-05 21:53:54 +0800

    限制飞船的移动范围

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game) -> None:
         """初始化飞船并设置其初始位置。"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
@@ -25,9 +26,9 @@ class Ship:
     def update(self):
         """根据移动标志调整飞船的位置。"""
         # 更新飞船而不是rect对象的x值
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # 根据self.x更新rect对象
