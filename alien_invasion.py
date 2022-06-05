a28e92be5e2e1288b0f3b977426d930bdbcb93d3
endcreeper861 <3130450154@qq.com>
2022-06-05 21:13:47 +0800

    重构run_game

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,24 +16,29 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((
             self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("外星人入侵")
 
         self.ship = Ship(self)
 
     def run_game(self):
         """开始游戏的主循环。"""
         while True:
-            # 监视鼠标和键盘事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
 
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+
+    def _update_screen(self):
+        """更新屏幕上的图象，并切换到新屏幕。"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+    def _check_events(self):
+        """响应鼠标和键盘事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == "__main__":
